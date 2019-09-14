73e0292b6f99133b1acfd48562c9f2b655ffd3be
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-14 17:17:50 +0200

    Plug the service into the controller

--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
@@ -1,11 +1,13 @@
 package io.arxiv.gumichan01.arxivspringpoc.controller
 
+import io.arxiv.gumichan01.arxivspringpoc.domain.service.ArxivService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ArxivController() {
+class ArxivController(@Autowired val arxivService: ArxivService) {
 
     @GetMapping("/")
     fun home(): String {
@@ -14,6 +16,7 @@ class ArxivController() {
 
     @GetMapping("/search/")
     fun search(@RequestParam(value = "q", required = true) query: String): String {
-        return "response"
+        val searchForResourceName = arxivService.searchForResourceName(query)
+        return searchForResourceName.toString()
     }
 }
