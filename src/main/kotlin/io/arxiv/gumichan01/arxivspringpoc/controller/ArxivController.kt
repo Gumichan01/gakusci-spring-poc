7b7e879aaf717129b2a619c71dc181289e197e71
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-14 17:13:33 +0200

    Format code

--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ArxivController() {
 
-     @GetMapping("/")
-     fun home(): String {
-         return "Hello Arxiv client\n"
-     }
+    @GetMapping("/")
+    fun home(): String {
+        return "Hello Arxiv client\n"
+    }
 
     @GetMapping("/search/")
     fun search(@RequestParam(value = "q", required = true) query: String): String {
