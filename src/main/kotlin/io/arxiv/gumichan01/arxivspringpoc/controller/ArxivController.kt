c7db959a2dca168345879e3ca9ad2a86a368d2ae
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-14 21:38:43 +0200

    Removed the autowired annotation - useless

--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
@@ -1,13 +1,12 @@
 package io.arxiv.gumichan01.arxivspringpoc.controller
 
 import io.arxiv.gumichan01.arxivspringpoc.domain.service.ArxivService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ArxivController(@Autowired val arxivService: ArxivService) {
+class ArxivController(val arxivService: ArxivService) {
 
     @GetMapping("/")
     fun home(): String {
--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/domain/service/ArxivService.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/domain/service/ArxivService.kt
@@ -2,18 +2,15 @@ package io.arxiv.gumichan01.arxivspringpoc.domain.service
 
 import io.arxiv.gumichan01.arxivspringpoc.domain.model.DocumentEntry
 import io.arxiv.gumichan01.arxivspringpoc.restapi.ArxivRestTemplate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.logging.Logger
 
 
 @Service
-class ArxivService(@Autowired val arxivRestTemplate: ArxivRestTemplate) {
+class ArxivService(val arxivRestTemplate: ArxivRestTemplate) {
 
     fun searchForResourceName(query: String): List<DocumentEntry> {
         val result = arxivRestTemplate.search(query)
-        val logger: Logger = Logger.getLogger(ArxivService::class.toString())
-        logger.info(result)
+        println(result)
         return listOf(DocumentEntry("hello", "#"))
     }
 }
