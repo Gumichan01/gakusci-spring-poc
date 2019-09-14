f6b00f186ddc56da5ce9e0a6c54806c2f8cad9dc
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-14 16:15:39 +0200

    Set minimal program

---
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/ArxivSpringPocApplication.kt
@@ -0,0 +1,11 @@
+package io.arxiv.gumichan01.arxivspringpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArxivSpringPocApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArxivSpringPocApplication>(*args)
+}
---
+++ src/test/kotlin/io/arxiv/gumichan01/arxivspringpoc/ArxivSpringPocApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.arxiv.gumichan01.arxivspringpoc
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ArxivSpringPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
