5d37194cc8a4513e75d3639076f2e9b728be76b9
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-14 17:13:16 +0200

    Add new route in the controller

--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/controller/ArxivController.kt
@@ -1,13 +1,19 @@
 package io.arxiv.gumichan01.arxivspringpoc.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ArxivController {
+class ArxivController() {
 
      @GetMapping("/")
      fun home(): String {
          return "Hello Arxiv client\n"
      }
+
+    @GetMapping("/search/")
+    fun search(@RequestParam(value = "q", required = true) query: String): String {
+        return "response"
+    }
 }
