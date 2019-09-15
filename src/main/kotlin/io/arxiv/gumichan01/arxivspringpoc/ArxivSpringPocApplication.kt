af5847dd481279bec363933cd4d938fea15642d8
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-15 10:42:52 +0200

    Clean code

--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/ArxivSpringPocApplication.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/ArxivSpringPocApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class ArxivSpringPocApplication
 
 fun main(args: Array<String>) {
-	runApplication<ArxivSpringPocApplication>(*args)
+    runApplication<ArxivSpringPocApplication>(*args)
 }
