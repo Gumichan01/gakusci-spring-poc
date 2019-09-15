d55c4552c52df0cd2e244634e017cfd5b9d8b838
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-15 10:39:34 +0200

    Refactor the search

--- src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/restapi/ArxivRestTemplate.kt
+++ src/main/kotlin/io/arxiv/gumichan01/arxivspringpoc/restapi/ArxivRestTemplate.kt
@@ -1,7 +1,5 @@
 package io.arxiv.gumichan01.arxivspringpoc.restapi
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
@@ -14,9 +12,7 @@ class ArxivRestTemplate {
 
     fun search(query: String): List<ArxivResultEntry> {
         val url = arxivUrl.format(query)
-        val jsonResponse = restTemplate.getForObject<String>(url)
-        val mapper = jacksonObjectMapper()
-        val arxivResult: ArxivResponse = mapper.readValue(jsonResponse, object : TypeReference<ArxivResponse>() {})
-        return arxivResult.response.docs ?: listOf()
+        val arxivResponse: ArxivResponse? = restTemplate.getForObject(url)
+        return arxivResponse?.response?.docs ?: listOf()
     }
 }
