3d07236cc32f029912b8058b171ad6c45aa3ff11
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-21 17:36:10 +0200

    Pretty print data from Arxiv

--- src/main/kotlin/io/gakusci/gumichan01/springpoc/restapi/Response.kt
+++ src/main/kotlin/io/gakusci/gumichan01/springpoc/restapi/Response.kt
@@ -1,12 +1,11 @@
 package io.gakusci.gumichan01.springpoc.restapi
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.rometools.rome.feed.synd.SyndPerson
-import io.gakusci.gumichan01.springpoc.utils.ImmutableList
 import java.util.*
 
 // HAL Archives ouvertes response data
 data class HalResponse(val response: HalResponseBody)
+
 data class HalResponseBody(val numFound: Int, val start: Int, val docs: List<HalResultEntry>?)
 
 data class HalResultEntry(val docid: Int, @JsonProperty("label_s") val label: String,
@@ -15,6 +14,12 @@ data class HalResultEntry(val docid: Int, @JsonProperty("label_s") val label: St
 // Arxiv response data
 
 // TODO define classes
-data class ArxivResultEntry(val authors: List<SyndPerson>, val title: String, val publishedDate: Date, val link: String)
+data class Author(val name: String) {
+    override fun toString() = name
+}
 
-fun ArxivResultEntry.label() : String = "$authors. $title. $publishedDate"
+// The display of the result should be normalized, no matter what the source is.
+// But well, It's a POC, it is not designed to be in production
+data class ArxivResultEntry(val authors: List<Author>, val title: String, val publishedDate: Date, val link: String)
+
+fun ArxivResultEntry.label(): String = "$authors. $title. publishedDate"
--- src/main/kotlin/io/gakusci/gumichan01/springpoc/utils/ImmutableList.kt
+++ src/main/kotlin/io/gakusci/gumichan01/springpoc/utils/ImmutableList.kt
@@ -1,3 +1,7 @@
 package io.gakusci.gumichan01.springpoc.utils
 
-class ImmutableList<T>(private val inner: MutableList<T>) : List<T> by inner
+class ImmutableList<T>(private val inner: MutableList<T>) : List<T> by inner {
+    override fun toString(): String {
+        return inner.map { e -> e.toString() }.reduce { acc, s -> acc + s }
+    }
+}
