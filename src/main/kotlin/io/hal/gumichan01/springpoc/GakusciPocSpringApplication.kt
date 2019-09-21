a011c12b61983e0c00041041b279fbd60b708259
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-21 12:17:46 +0200

    Rename the application class

--- src/main/kotlin/io/hal/gumichan01/springpoc/GakusciPocSpringApplication.kt
+++ src/main/kotlin/io/hal/gumichan01/springpoc/GakusciPocSpringApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ArxivSpringPocApplication
+class GakusciPocSpringApplication
 
 fun main(args: Array<String>) {
-    runApplication<ArxivSpringPocApplication>(*args)
+    runApplication<GakusciPocSpringApplication>(*args)
 }
--- src/test/kotlin/io/hal/gumichan01/springpoc/GakusciPocSpringApplicationTests.kt
+++ src/test/kotlin/io/hal/gumichan01/springpoc/GakusciPocSpringApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class ArxivSpringPocApplicationTests {
+class GakusciPocSpringApplicationTests {
 
 	@Test
 	fun contextLoads() {
