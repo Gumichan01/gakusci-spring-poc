d0f32f9ed688b3e5c993f7a5582439b172a97031
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-10-14 01:24:52 +0200

    Return the model instead of the text
    I don't understand everything but it seems that if you do not return the model,
    then the page is not displayed.
    
    I also set the page template.

--- src/main/kotlin/io/gakusci/gumichan01/springpoc/controller/Controller.kt
+++ src/main/kotlin/io/gakusci/gumichan01/springpoc/controller/Controller.kt
@@ -1,7 +1,7 @@
 package io.gakusci.gumichan01.springpoc.controller
 
-import io.gakusci.gumichan01.springpoc.domain.model.DocumentEntry
 import io.gakusci.gumichan01.springpoc.domain.service.SearchAggregator
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,12 +9,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class Controller(val searchAggregator: SearchAggregator) {
 
-    @GetMapping("/search/")
-    fun search(@RequestParam(value = "q", required = true) query: String): List<DocumentEntry> {
-        // TODO Create a simple Service Handler that handles the two services
-        // TODO -> IService is important
-        // TODO This service can launch any number of services in concurrently / or even in parrallel
-        // TODO asynchronously
-        return searchAggregator.search(query)
+    @GetMapping("/search")
+    fun search(@RequestParam(value = "q", required = true) query: String, model: Model): Model {
+        val results = searchAggregator.search(query)
+        model.addAttribute("entries", results)
+        return model
     }
 }
